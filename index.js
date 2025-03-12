d4d1b97f74f1c71fbe12382720b997172d47356e
manikanta201104 <manikanta0224402@gmail.com>
2025-03-12 21:39:21 +0530

    updated

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://filesharing-backend-6uqo.onrender.com";
+const baseURL = "https://innshare.herokuapp.com";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
